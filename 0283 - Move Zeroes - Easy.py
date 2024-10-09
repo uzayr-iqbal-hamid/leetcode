5405ee2d3d7fc01c077d0593d014fc3064f3ee98
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-10-10 00:10:57 +0530

    Update 0283 - Move Zeroes - Easy.py

--- 0283 - Move Zeroes - Easy.py
+++ 0283 - Move Zeroes - Easy.py
@@ -11,3 +11,17 @@ class Solution:
         for i in range(noOfZero):
             nums.remove(0)
             nums.append(0)
+
+
+# alternate solution
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
