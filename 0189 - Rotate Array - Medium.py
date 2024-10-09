bf569e25a26c141903a39fc1166fa7513a78a369
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-10-09 23:39:10 +0530

    Create 0189 - Rotate Array - Medium.py

---
+++ 0189 - Rotate Array - Medium.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        k = k % len(nums)
+        nums.reverse()
+
+        nums[ : k ] = reversed(nums[ : k ])
+
+        nums[ k : ] = reversed(nums[ k : ])
