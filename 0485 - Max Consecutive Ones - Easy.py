51a7f2693c226395c94bea7bcebcc4ec3be8a29b
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-10-10 12:47:05 +0530

    Update 0485 - Max Consecutive Ones - Easy.py

--- 0485 - Max Consecutive Ones - Easy.py
+++ 0485 - Max Consecutive Ones - Easy.py
@@ -11,3 +11,24 @@ class Solution:
             ans.append(count)
         
         return max(ans)
+
+# Alternate solution:
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        i = 0
+        
+        max_count = 0
+        while i < len(nums):
+            count = 0
+            if nums[i] == 1:
+                while i < len(nums) and nums[i] == 1: 
+                    count += 1
+                    i += 1
+                        
+                if count > max_count:
+                    max_count = count
+            else:
+                i += 1
+
+        return max_count
