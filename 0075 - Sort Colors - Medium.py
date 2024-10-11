5928c8b581e26d3f87f2809d0288bf41b5c1cc99
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-10-12 02:08:13 +0530

    Update 0075 - Sort Colors - Medium.py

--- 0075 - Sort Colors - Medium.py
+++ 0075 - Sort Colors - Medium.py
@@ -1,7 +1,41 @@
+"""
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+Constraints:
+n == nums.length
+1 <= n <= 300
+nums[i] is either 0, 1, or 2.
+"""
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        counter = {
+            0: 0,
+            1: 0,
+            2: 0
+        }
 
-        nums.sort()
+        for i in nums:
+            counter[i] += 1
+
+        index = 0
+        for color in range(3):
+            for _ in range(counter[color]):
+                nums[index] = color
+                index += 1
