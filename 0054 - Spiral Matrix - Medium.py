4efe14dd1cef9c0a4b5d9af42a35fd1cff303613
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-10-20 11:05:56 +0530

    Create 0054 - Spiral Matrix - Medium.py

---
+++ 0054 - Spiral Matrix - Medium.py
@@ -0,0 +1,49 @@
+"""
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+
+Example 2:
+Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ 
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+"""
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        top = 0
+        bottom = m - 1
+        left = 0
+        right = n - 1
+        res = []
+
+        while (left <= right) and (top <= bottom): 
+            for i in range(left, right+1):
+                res.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom+1):
+                res.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    res.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    res.append(matrix[i][left])
+                left += 1
+        
+        return res
