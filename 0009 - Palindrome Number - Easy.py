5079938df48f256eefe766123a7574e8b7b08d8c
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-10-22 19:52:15 +0530

    Update 0009 - Palindrome Number - Easy.py

--- 0009 - Palindrome Number - Easy.py
+++ 0009 - Palindrome Number - Easy.py
@@ -1,3 +1,27 @@
+"""
+Given an integer x, return true if x is a 
+palindrome
+, and false otherwise.
+
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Constraints:
+-231 <= x <= 231 - 1
+"""
+
 class Solution:
     def isPalindrome(self, x: int):
 
@@ -11,3 +35,9 @@ class Solution:
             return True
         else:
             return False
+
+# Alternate solution
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return True if str(x) == str(x)[::-1] else False
