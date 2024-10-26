42de832e5886edb32959587187ef0ce3996e1c30
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-10-26 15:24:54 +0530

    Create 1021 - Remove Outermost Parantheses - Easy.py

---
+++ 1021 - Remove Outermost Parantheses - Easy.py
@@ -0,0 +1,52 @@
+"""
+A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+ 
+Example 1:
+Input: s = "(()())(())"
+Output: "()()()"
+Explanation: 
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+
+Example 2:
+Input: s = "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation: 
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+
+Example 3:
+Input: s = "()()"
+Output: ""
+Explanation: 
+The input string is "()()", with primitive decomposition "()" + "()".
+After removing outer parentheses of each part, this is "" + "" = "".
+ 
+Constraints:
+1 <= s.length <= 105
+s[i] is either '(' or ')'.
+s is a valid parentheses string.
+"""
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        res = ""
+
+        valid = 0
+        
+        for char in s:
+            if char == '(':
+                if valid > 0:
+                    res += char
+                valid += 1
+                
+            elif char == ')':
+                valid -= 1
+                if valid > 0:
+                    res += char
+                
+        return res
