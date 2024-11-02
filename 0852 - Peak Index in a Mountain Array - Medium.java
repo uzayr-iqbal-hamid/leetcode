cd1140195aaceae6137641a024ab4ce96c4eb773
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-11-02 12:00:02 +0530

    Create 0852 - Peak Index in a Mountain Array - Medium.java

---
+++ 0852 - Peak Index in a Mountain Array - Medium.java
@@ -0,0 +1,42 @@
+"""
+You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+Return the index of the peak element.
+Your task is to solve it in O(log(n)) time complexity.
+
+Example 1:
+Input: arr = [0,1,0]
+Output: 1
+
+Example 2:
+Input: arr = [0,2,1,0]
+Output: 1
+
+Example 3:
+Input: arr = [0,10,5,2]
+Output: 1
+
+Constraints:
+3 <= arr.length <= 105
+0 <= arr[i] <= 106
+arr is guaranteed to be a mountain array.
+"""
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
