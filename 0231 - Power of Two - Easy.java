828c29526dea012e4d9635295b400a5978e0ba54
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-11-25 20:25:41 +0530

    Create 0231 - Power of Two - Easy.java

---
+++ 0231 - Power of Two - Easy.java
@@ -0,0 +1,16 @@
+// Recursive Approach
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 1) {
+            return true;
+        }
+        
+        if (n <= 0 || n % 2 != 0) {
+            return false;
+        }
+        
+        return isPowerOfTwo(n / 2);
+    }
+    
+}
