2c5fc2f33fd9e18676675790272d7b8463b04008
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2024-11-25 20:29:43 +0530

    Create 0326 - Power of Three - Easy.java

---
+++ 0326 - Power of Three - Easy.java
@@ -0,0 +1,14 @@
+// Recursive Approach
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n == 1) {
+            return true;
+        }
+        if (n <= 0 || n % 3 != 0) {
+            return false;
+        }
+
+        return isPowerOfThree(n / 3);
+    }
+}
