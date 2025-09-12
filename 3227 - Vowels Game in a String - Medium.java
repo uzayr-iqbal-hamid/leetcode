d82f05569b6f02ec891c06e442e3f3d2d7b7f8df
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2025-09-13 02:33:37 +0530

    Create 3227 - Vowels Game in a String - Medium.java

---
+++ 3227 - Vowels Game in a String - Medium.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesAliceWin(String s) {
+
+        for (char c : s.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
