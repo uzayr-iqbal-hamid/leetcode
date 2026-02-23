0f9cb47bb6efb7e6c3c15e7e5c225ff0a2d7dcb2
Uzayr Iqbal Hamid <134723279+uzayr-iqbal-hamid@users.noreply.github.com>
2026-02-23 16:05:58 +0530

    Topics: Array

---
+++ 0941 - Valid Mountain Array - Easy.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int n = arr.length;
+        if (n < 3) return false;
+
+        int i = 0;
+        while (i + 1 < n && arr[i] < arr[i + 1]) i++;
+
+        if (i == 0 || i == n - 1) return false;
+
+        while (i + 1 < n && arr[i] > arr[i + 1]) i++;
+
+        return i == n - 1;
+    }
+}
